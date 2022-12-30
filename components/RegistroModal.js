95040728840c7b76ad9ed9376629e9ed6b549555
Luis <lapcpc@hotmail.com>
2022-12-30 11:39:50 -0600

    modal finished

--- components/RegistroModal.js
+++ components/RegistroModal.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function RegistroModal() {
-
+function RegistroModal({estado, accion}) {
+    const hijo =(e) =>{
+        e.preventDefault();
+        e.stopPropagation();
+    }
   return (
-    <div className='absolute w-screen h-screen bg-opacity-50 bg-gray-500 '>
-        <div className='p-2 shadow-lg rounded-md relative top-[30%] left-[15%] sm:left-[20%] md:left-[25%] lg:left-[35%] w-2/3 sm:w-6/12 md:w-5/12 lg:w-4/12 bg-white '>
-           <h1 className='text-center p-2 text-2xl font-semibold'>Registro</h1> 
-           <form className='flex flex-col space-y-2'>
-            <input className='w-full p-2 outline-none focus:border focus:border-sky-500  placeholder:' placeholder='Name' />
-            <input className='w-full p-2 outline-none focus:border focus:border-sky-500  placeholder:' placeholder='Email'/>
-            <input type="password" className='w-full p-2 outline-none focus:border focus:border-sky-500  placeholder:' placeholder='Password'/>
-            <button className='bg-green-500 w-1/3 mx-auto p-2 rounded-lg font-semibold text-white'>Registrate</button>
+    <div onClick={accion} className={ `${estado ? 'hidden':'absolute'} w-screen h-screen bg-opacity-50 bg-gray-500 `}>
+        <div onClick={hijo} className='p-2 shadow-lg rounded-md relative top-[30%] left-[15%] sm:left-[20%] md:left-[25%] lg:left-[35%] w-2/3 sm:w-6/12 md:w-5/12 lg:w-4/12 bg-white '>
+           <h1 className='text-center p-2 text-2xl font-semibold'>Create user</h1> 
+           <form className='flex flex-col space-y-2 p-2 justify-center'>
+           <div className='flex flex-col w-full mx-auto space-y-2'>
+                <input className='mx-auto w-11/12 rounded-md p-2 outline-none border border-slate-200  focus:border-sky-500  placeholder:' placeholder='Name' />
+                <input className='mx-auto w-11/12 rounded-md p-2 outline-none border border-slate-200  focus:border-sky-500  placeholder:' placeholder='Email'/>
+                <input type="password" className='mx-auto w-11/12 rounded-md   p-2 outline-none border border-slate-200 focus:border-sky-500  placeholder:' placeholder='Password'/>
+           </div>
+            <br />
+            <button className='bg-green-500 w-1/3 mx-auto p-2 rounded-lg font-semibold text-white'>Register Now</button>
            </form>
 
         </div>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,19 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import RegistroModal from '../components/RegistroModal'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  
+  const [visibilidad, setVisibilidad] = useState(true)
+  const changeState = () => {
+    setVisibilidad(!visibilidad)
+  }
   return (
    <div className='bg-gray-100 flex flex-row  space-x-10  justify-center h-screen'>
-      <RegistroModal />
+      <RegistroModal estado={visibilidad} accion={changeState} />
       <div className='my-auto flex flex-col justify-center '>
         <div className='flex justify-center'>
         <img className=' w-32 h-32' src='https://cdn-icons-png.flaticon.com/512/2510/2510074.png' />
@@ -25,19 +31,19 @@ export default function Home() {
        
     </div>
     <div className=' border border-slate-200 w-[25%] p-2 bg-white my-auto flex flex-row  rounded-lg shadow-xl'>
-            <form className='w-full'>
+            <div className='w-full'>
 
-              <input className='focus:outline-none focus:border-blue-500 focus:border rounded-lg focus:shadow-sm focus:shadow-blue-500/50  w-11/12 placeholder:min-w-full m-2 p-2' placeholder='Correo electronico o numero de teléfono' />
+              <input className='focus:outline-none focus:border-blue-500 focus:border rounded-lg focus:shadow-sm focus:shadow-blue-500/50  w-11/12 placeholder:min-w-full m-2 p-2' placeholder='Username' />
               <br></br>
-              <input className='focus:outline-none focus:border-blue-500 focus:border rounded-lg focus:shadow-sm focus:shadow-blue-500/50   w-11/12 placeholder:min-w-full m-2 p-2' placeholder='Contraseña' />
+              <input className='focus:outline-none focus:border-blue-500 focus:border rounded-lg focus:shadow-sm focus:shadow-blue-500/50   w-11/12 placeholder:min-w-full m-2 p-2' placeholder='Password' />
               <br></br>
-              <button className=' bg-blue-500 hover:bg-blue-500/50 text-white w-11/12 rounded-md text-xl font-medium placeholder:min-w-full m-1 p-2.5'>Iniciar sesión</button>
-              <p className='text-blue-600 text-center w-11/12 placeholder:min-w-full m-2 p-2'>¿Olvidaste tu contraseña?</p>
+              <button className=' bg-blue-500 hover:bg-blue-500/50 text-white w-11/12 rounded-md text-xl font-medium placeholder:min-w-full m-1 p-2.5'>Sign In</button>
+              <p className='text-blue-600 text-center w-11/12 placeholder:min-w-full m-2 p-2'>Forgot password?</p>
               <hr className='mb-3' />
               <div className='flex flex-row'>
-                <button className='text-white bg-green-500 hover:bg-green-500/50 w-6/12 rounded-lg font-bold placeholder:min-w-full mx-auto my-1 p-3'>Crear cuenta nueva</button> 
+                <button onClick={() => setVisibilidad(!visibilidad)} className='text-white bg-green-500 hover:bg-green-500/50 w-6/12 rounded-lg font-bold placeholder:min-w-full mx-auto my-1 p-3'>Create account</button> 
               </div>
-            </form>
+            </div>
       </div>
    </div>
   )
